05d03beb44b705560013bade0879255e426e9b9b
EA1A87 <163140@autistici.org>
2020-08-29 15:04:23 +1000

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,8 @@
+PROJECT = point
+PROJECT_DESCRIPTION = Библиотека для манипулирования точками в плоскости
+PROJECT_VERSION = 0.0.0
+
+DEPS = line
+dep_line = git http://some.where
+
+include erlang.mk
---
+++ src/point.erl
@@ -0,0 +1,92 @@
+-module(point).
+-include_lib("eunit/include/eunit.hrl").
+-export([at_line/2, shift/2, sum/2, neg/1, mirror/2, str/1]).
+-author("ea1a87").
+
+
+-type point():: {point, float(), float()}	. %% Точка в двухмерном пространстве
+-type line() ::	{line, point(), point()}	.	%% Прямая на плоскости проходящая заданная двумя точками
+
+%% @doc Возвращает новую точку заданную координатами X,Y
+-spec new( number(), number() ) -> point().
+new(X,Y)		-> {point, float(X), float(Y)}.
+new_test()	-> ?_assert(new(10,30) =:= {point, 10, 30}).
+
+%% TODO: UNTESTED
+%% @doc Проверяет лежит ли точка на прямой. Возвращает true/false
+-spec at_line( Line::line(), A::point() ) -> atom(). 
+at_line(Line, A)	 ->
+	{point, Xa, Ya} = A,
+	Y = line:findY(Line, Xa),
+	if
+		Y/=Ya -> false;
+		true	-> true
+	end
+.
+at_line_test_()		-> [
+	?_assert(at_line(	{ line, {point,  0, 0},{point,-10, 10}},{point,-5, 5}) =:= true ),
+	?_assert(at_line(	{ line, {point, 10, 0},{point, 20, 10}},{point,15, 5}) =:= true ),
+	?_assert(at_line(	{ line, {point, 10, 0},{point, 20, 10}},{point,16, 5}) =:= false)
+].
+
+%% @doc			Складывает две точки. Возвращает точку с координатами (X1+X2,Y1+Y2).
+-spec shift( A::point(), B::point() )->point().
+shift(A, B)	 ->
+	{point, Xa, Ya} = A,
+	{point, Xb, Yb} = B,
+	{point, Xa+Xb, Ya+Yb}.
+shift_test() -> ?_assert( shift( {point, 30, 72.3},{point,-30.75, 9} ) =:= {point, -0.75, 81.3} ).
+
+%% @doc			Складывает две точки. Возвращает точку с координатами (X1+X2,Y1+Y2).
+-spec sum( A::point(), B::point() )->point().
+sum(A, B) -> shift(A, B).
+
+
+%% @doc			Зеркалирует точку относительно начала координат / Умножает точку на -1. Возвращает точку с координатами (-X,-Y).
+-spec neg( point() )->point().
+neg(A)		 ->
+	{point, X, Y} = A,
+	{point, -X, -Y}.
+neg_test() -> ?_assert( neg({point, 15, 3}) =:= {point, -15, -3} ).
+
+%% @doc			Отзеркаливает точку относительно прямой. Точку с координатами (X1, Y1) зеркальную заданной. 
+%% 					Подробности на поясняющем рисунке
+-spec mirror( line(), point() ) -> point().
+mirror(Line, A) ->
+	{point, Xa, Ya}= A,
+	C1	= line:intersection(Line, {line, {point, 0, Ya}, A}),
+	C2	= line:intersection(Line, {line, {point, Xa, 0}, A}),
+	AC1	= line:len({line, A, C1}),
+	AC2	= line:len({line, A, C2}),
+	A		= math:atan(AC1/AC2),
+	DX			= AC1 * math:sin(A)*direction(x, A, C1).
+	DY			= math:sqrt(4 * AC1 * AC1 * math:pow(math:sin(A), 2) - (DX * DX )) * direction(y, A, C2),
+	shift(A, {point, DX, DY}).
+mirror_test_()	-> [
+	?_assert(at_line(	{ line, {point,  0, 0},{point,100,100} },{point, 50, 40}) =:= {point, 40, 50} ),
+	?_assert(at_line(	{ line, {point,  0, 0},{point,-10,-10} },{point,-40,-80}) =:= {point,-80,-40} )
+].
+
+%% @doc Направление отрезка по соответствующей оси
+direction(x, A, B) ->
+	{point, Xa, _ } = A,
+	{point, Xb, _ } = B,
+	(Xb-Xa)/abs(Xa-Xb);
+direction(y, A, B) ->
+	{point, _, Ya } = A,
+	{point, _, Yb } = B,
+	(Yb-Ya)/abs(Ya-Yb).
+direction_test_() -> [
+	?assert(direction( x, { point, 10, 10}, {point, 20, 20} ) =:= 1 ),
+	?assert(direction( x, { point, 10, 10}, {point, 00, 20} ) =:=-1 ),
+	?assert(direction( x, { point, 10, 10}, {point, 10, 20} ) =:= 0 ),
+	?assert(direction( y, { point, 10, 10}, {point, 10, 20} ) =:= 1 ),
+	?assert(direction( y, { point, 10, 10}, {point, 10, 00} ) =:=-1 ),
+	?assert(direction( y, { point, 10, 10}, {point, 30, 10} ) =:= 0 )
+
+%% @doc Переводит точку в обычную (небинарную) строку вида "X,Y".
+-spec str( point() ) -> string() .
+str(A)		 ->
+ {point, X, Y} = A,
+ [round(X), ",", round(Y)].
+str_test() -> ?_assert({point, 9666, 0.13}) =:= [9666, ",", 0.13]).
